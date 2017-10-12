ea525ab156cbb305e55eb10c09d8eb49b23e1956
Michael Cotter <20078933@wit.ie>
2017-10-12 18:27:54 +0100

    Added tests for movies_findMany function

--- test/routes/movie_functions-test.js
+++ test/routes/movie_functions-test.js
@@ -79,5 +79,107 @@ describe('Movie Functions', function (){
                 });
             });
         });
+        describe('\n      GET   /movies/multiple/search   function:findMany [requires the inclusion of request parameters]', function(){
+            describe('/movies/multiple/search?ids=59eb66125b06692facbcd437%2C59eb66125b06692facbcd438',function(){
+                it('should return an array of Movie objects whose IDs match those sent as request parameters',function(done){
+                    supertest
+                        .get('/movies/multiple/search?ids=59eb66125b06692facbcd437%2C59eb66125b06692facbcd438')
+                        .end(function(err,res){
+                            expect(res.status).equal(200);
+                            expect(res.body).to.be.a('array');
+                            expect(res.body.length).equal(2);
+                            var result = _.map(res.body, function (movie) {
+                                return { name: movie.name, year: movie.year, genre: movie.genre, type: movie.type, rating: movie.rating, content_rating: movie.content_rating, cast_and_crew: movie.cast_and_crew }
+                            });
+                            expect(result).to.include( {name:"Blade Runner",year:1982,genre:"Sci-fi Detective",type:"feature",rating:5,content_rating:"R",cast_and_crew:["Harrison Ford","Rutger Hauer","Ridley Scott","Vangelis","Hampton Fancher","Jordan Cronenweth"]} );
+                            expect(result).to.include( {name:"No Country for Old Men",year:2007,genre:"Western Drama",type:"feature",rating:9,content_rating:"R",cast_and_crew:["Josh Brolin","Tommy Lee Jones","Javier Bardem","Kelly MacDonald","Woody Harrelson","Joel Coen","Ethan Coen","Roger Deakins"]} );
+                            done();
+                        });
+                });
+            });
+            describe('/movies/multiple/search?ids=59eb66125b06692facbcd437',function(){
+                it('should return an array containing a single Movie object, the ID of which was sent as a request parameter',function(done){
+                    supertest
+                        .get('/movies/multiple/search?ids=59eb66125b06692facbcd437')
+                        .end(function(err,res){
+                            expect(res.status).equal(200);
+                            expect(res.body).to.be.a('array');
+                            expect(res.body.length).equal(1);
+                            var result = _.map(res.body, function (movie) {
+                                return { name: movie.name, year: movie.year, genre: movie.genre, type: movie.type, rating: movie.rating, content_rating: movie.content_rating, cast_and_crew: movie.cast_and_crew }
+                            });
+                            expect(result).to.include( {name:"Blade Runner",year:1982,genre:"Sci-fi Detective",type:"feature",rating:5,content_rating:"R",cast_and_crew:["Harrison Ford","Rutger Hauer","Ridley Scott","Vangelis","Hampton Fancher","Jordan Cronenweth"]} );
+                            done();
+                        });
+                });
+            });
+            describe('/movies/multiple/search?ids=59eb66125b06692facbcd437%2C1234',function(){
+                it('should return an array containing as many Movie objects as there were valid IDs sent as request parameters',function(done){
+                    supertest
+                        .get('/movies/multiple/search?ids=59eb66125b06692facbcd437%2C1234')
+                        .end(function(err,res){
+                            expect(res.status).equal(200);
+                            expect(res.body).to.be.a('array');
+                            expect(res.body.length).equal(1);
+                            var result = _.map(res.body, function (movie) {
+                                return { name: movie.name, year: movie.year, genre: movie.genre, type: movie.type, rating: movie.rating, content_rating: movie.content_rating, cast_and_crew: movie.cast_and_crew }
+                            });
+                            expect(result).to.include( {name:"Blade Runner",year:1982,genre:"Sci-fi Detective",type:"feature",rating:5,content_rating:"R",cast_and_crew:["Harrison Ford","Rutger Hauer","Ridley Scott","Vangelis","Hampton Fancher","Jordan Cronenweth"]} );
+                            done();
+                        });
+                });
+            });
+            describe('/movies/multiple/search?ids=59eb66125b06692facbcd437345345342C1234',function(){
+                it('should return an empty array when no valid Movie Object IDs were sent as request parameters',function(done){
+                    supertest
+                        .get('/movies/multiple/search?ids=59eb66125b06692facbcd437345345342C1234')
+                        .end(function(err,res){
+                            expect(res.status).equal(404);
+                            expect(res.body).to.have.property('message').equal("No Movie Objects were found.");
+                            expect(res.body.array).to.be.a('array');
+                            expect(res.body.array.length).equal(0);
+                            //var result = _.map(res.body, function (movie) {
+                            //return { name: movie.name, year: movie.year, genre: movie.genre, type: movie.type, rating: movie.rating, content_rating: movie.content_rating, cast_and_crew: movie.cast_and_crew }
+                            //});
+                            //expect(result).to.include( {name:"Blade Runner",year:1982,genre:"Sci-fi Detective",type:"feature",rating:5,content_rating:"R",cast_and_crew:["Harrison Ford","Rutger Hauer","Ridley Scott","Vangelis","Hampton Fancher","Jordan Cronenweth"]} );
+                            done();
+                        });
+                });
+            });
+            describe('/movies/multiple/search?something=59eb66125b06692facbcd437345345342C1234',function(){
+                it('should return an error when request parameters are incorrectly labelled',function(done){
+                    supertest
+                        .get('/movies/multiple/search?something=59eb66125b06692facbcd437345345342C1234')
+                        .end(function(err,res){
+                            expect(res.status).equal(404);
+                            expect(res.body).to.have.property('message').equal("The request parameters needs to be labelled ids.");
+                            done();
+                        });
+                });
+            });
+            describe('/movies/multiple/search?ids=',function(){
+                it('should return an error when no request parameters are are included after ids=',function(done){
+                    supertest
+                        .get('/movies/multiple/search?ids=')
+                        .end(function(err,res){
+                            expect(res.status).equal(404);
+                            expect(res.body).to.have.property('message').equal("No ID values were included as request parameters.");
+                            done();
+                        });
+                });
+            });
+            describe('/movies/multiple/search',function(){
+                it('should return an error when request path is invalid',function(done){
+                    supertest
+                        .get('/movies/multiple/search')
+                        .end(function(err,res){
+                            expect(res.status).equal(404);
+                            expect(res.body).to.have.property('message').equal("You forgot to include request parameters.");
+                            //expect(res.body).to.have.property('message').equal('The request parameters needs to be labelled ids.');
+                            done();
+                        });
+                });
+            });
+        });
     });
 });
