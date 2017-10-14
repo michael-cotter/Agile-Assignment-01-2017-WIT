3f252e5bb6b8fa1b2ef7a4d1f6c6523a70d4604c
Michael Cotter <20078933@wit.ie>
2017-10-14 10:03:55 +0100

    Added tests for movies_substringSearch function

--- test/routes/movie_functions-test.js
+++ test/routes/movie_functions-test.js
@@ -181,5 +181,196 @@ describe('Movie Functions', function (){
                 });
             });
         });
+        describe('\n      GET   /movies/fuzzy/*attribute*/:substring_input   function:substringSearch', function(){
+            describe('/movies/fuzzy/name/r',function(){
+                it('should return all Movie objects, the names of which contain :substring_input as a substring, regardless of case', function(done){
+                    supertest
+                        .get('/movies/fuzzy/name/r')
+                        .end(function(err,res){
+                            expect(res.status).equal(200);
+                            expect(res.body).to.be.a('array');
+                            expect(res.body.length).equal(2);
+                            var result = _.map(res.body, function (movie) {
+                                return { name: movie.name, year: movie.year, genre: movie.genre, type: movie.type, rating: movie.rating, content_rating: movie.content_rating, cast_and_crew: movie.cast_and_crew }
+                            });
+                            expect(result).to.include({name:"Blade Runner",year:1982,genre:"Sci-fi Detective",type:"feature",rating:5,content_rating:"R",cast_and_crew:["Harrison Ford","Rutger Hauer","Ridley Scott","Vangelis","Hampton Fancher","Jordan Cronenweth"]});
+                            expect(result).to.include({name:"No Country for Old Men",year:2007,genre:"Western Drama",type:"feature",rating:9,content_rating:"R",cast_and_crew:["Josh Brolin","Tommy Lee Jones","Javier Bardem","Kelly MacDonald","Woody Harrelson","Joel Coen","Ethan Coen","Roger Deakins"]});
+                            done();
+                        });
+                });
+            });
+            describe('/movies/fuzzy/name/No Country for Old Men',function(){
+                it('should return a Movie object, the name of which is the exactly the same as :substring_input, regardless of case', function(done) {
+                    supertest
+                        .get('/movies/fuzzy/name/No Country for Old Men')
+                        .end(function(err,res){
+                            expect(res.status).equal(200);
+                            expect(res.body).to.be.a('array');
+                            expect(res.body.length).equal(1);
+                            var result = _.map(res.body, function (movie) {
+                                return { name: movie.name, year: movie.year, genre: movie.genre, type: movie.type, rating: movie.rating, content_rating: movie.content_rating, cast_and_crew: movie.cast_and_crew }
+                            });
+                            //expect(result).to.include({name:"Blade Runner",year:1982,genre:"Sci-fi Detective",type:"feature",rating:5,content_rating:"R",cast_and_crew:["Harrison Ford","Rutger Hauer","Ridley Scott","Vangelis","Hampton Fancher","Jordan Cronenweth"]});
+                            expect(result).to.include({name:"No Country for Old Men",year:2007,genre:"Western Drama",type:"feature",rating:9,content_rating:"R",cast_and_crew:["Josh Brolin","Tommy Lee Jones","Javier Bardem","Kelly MacDonald","Woody Harrelson","Joel Coen","Ethan Coen","Roger Deakins"]});
+                            done();
+                        });
+                });
+            });
+            describe('/movies/fuzzy/name/No%20Country%20for%20Old%20Men',function(){
+                it('should return a Movie object, the name of which is the exactly the same as :substring_input, regardless of case', function(done) {
+                    supertest
+                        .get('/movies/fuzzy/name/No%20Country%20for%20Old%20Men')
+                        .end(function(err,res){
+                            expect(res.status).equal(200);
+                            expect(res.body).to.be.a('array');
+                            expect(res.body.length).equal(1);
+                            var result = _.map(res.body, function (movie) {
+                                return { name: movie.name, year: movie.year, genre: movie.genre, type: movie.type, rating: movie.rating, content_rating: movie.content_rating, cast_and_crew: movie.cast_and_crew }
+                            });
+                            //expect(result).to.include({name:"Blade Runner",year:1982,genre:"Sci-fi Detective",type:"feature",rating:5,content_rating:"R",cast_and_crew:["Harrison Ford","Rutger Hauer","Ridley Scott","Vangelis","Hampton Fancher","Jordan Cronenweth"]});
+                            expect(result).to.include({name:"No Country for Old Men",year:2007,genre:"Western Drama",type:"feature",rating:9,content_rating:"R",cast_and_crew:["Josh Brolin","Tommy Lee Jones","Javier Bardem","Kelly MacDonald","Woody Harrelson","Joel Coen","Ethan Coen","Roger Deakins"]});
+                            done();
+                        });
+                });
+            });
+            describe('/movies/fuzzy/name/something_else',function(){
+                it('should return an error message when :substring_input is not a substring of the names of any Movie object', function(done){
+                    supertest
+                        .get('/movies/fuzzy/name/something_else')
+                        .end(function(err,res){
+                            expect(res.status).equal(404);
+                            expect(res.body).to.be.a('object');
+                            expect(res.body).to.have.property('message');
+                            expect(res.body.message).equal("The substring search failed to return any Movie objects");
+                            done();
+                        });
+                });
+            });
+            describe('/movies/fuzzy/genre/West',function(){
+                it('should return all Movie objects, the genres of which contain :substring_input as a substring, regardless of case', function(done){
+                    supertest
+                        .get('/movies/fuzzy/genre/West')
+                        .end(function(err,res){
+                            expect(res.status).equal(200);
+                            expect(res.body).to.be.a('array');
+                            expect(res.body.length).equal(1);
+                            var result = _.map(res.body, function (movie) {
+                                return { name: movie.name, year: movie.year, genre: movie.genre, type: movie.type, rating: movie.rating, content_rating: movie.content_rating, cast_and_crew: movie.cast_and_crew }
+                            });
+                            expect(result).to.include({name:"No Country for Old Men",year:2007,genre:"Western Drama",type:"feature",rating:9,content_rating:"R",cast_and_crew:["Josh Brolin","Tommy Lee Jones","Javier Bardem","Kelly MacDonald","Woody Harrelson","Joel Coen","Ethan Coen","Roger Deakins"]});
+                            done();
+                        });
+                });
+            });
+            describe('/movies/fuzzy/genre/Western Drama',function(){
+                it('should return Movie object(s), the genre(s) of which is/are the exactly the same as :substring_input, regardless of case', function(done) {
+                    supertest
+                        .get('/movies/fuzzy/genre/Western Drama')
+                        .end(function(err,res){
+                            expect(res.status).equal(200);
+                            expect(res.body).to.be.a('array');
+                            expect(res.body.length).equal(1);
+                            var result = _.map(res.body, function (movie) {
+                                return { name: movie.name, year: movie.year, genre: movie.genre, type: movie.type, rating: movie.rating, content_rating: movie.content_rating, cast_and_crew: movie.cast_and_crew }
+                            });
+                            //expect(result).to.include({name:"Blade Runner",year:1982,genre:"Sci-fi Detective",type:"feature",rating:5,content_rating:"R",cast_and_crew:["Harrison Ford","Rutger Hauer","Ridley Scott","Vangelis","Hampton Fancher","Jordan Cronenweth"]});
+                            expect(result).to.include({name:"No Country for Old Men",year:2007,genre:"Western Drama",type:"feature",rating:9,content_rating:"R",cast_and_crew:["Josh Brolin","Tommy Lee Jones","Javier Bardem","Kelly MacDonald","Woody Harrelson","Joel Coen","Ethan Coen","Roger Deakins"]});
+                            done();
+                        });
+                });
+            });
+            describe('/movies/fuzzy/genre/Incorrect Genre',function(){
+                it('should return an error message when :substring_input is not a substring of the genres of any Movie object', function(done){
+                    supertest
+                        .get('/movies/fuzzy/genre/Incorrect Genre')
+                        .end(function(err,res){
+                            expect(res.status).equal(404);
+                            expect(res.body).to.be.a('object');
+                            expect(res.body).to.have.property('message');
+                            expect(res.body.message).equal("The substring search failed to return any Movie objects");
+                            done();
+                        });
+                });
+            });
+            describe('/movies/fuzzy/type/f',function(){
+                it('should return all Movie objects, the types of which contain :substring_input as a substring, regardless of case', function(done){
+                    supertest
+                        .get('/movies/fuzzy/type/f')
+                        .end(function(err,res){
+                            expect(res.status).equal(200);
+                            expect(res.body).to.be.a('array');
+                            expect(res.body.length).equal(2);
+                            var result = _.map(res.body, function (movie) {
+                                return { name: movie.name, year: movie.year, genre: movie.genre, type: movie.type, rating: movie.rating, content_rating: movie.content_rating, cast_and_crew: movie.cast_and_crew }
+                            });
+                            expect(result).to.include({name:"Blade Runner",year:1982,genre:"Sci-fi Detective",type:"feature",rating:5,content_rating:"R",cast_and_crew:["Harrison Ford","Rutger Hauer","Ridley Scott","Vangelis","Hampton Fancher","Jordan Cronenweth"]});
+                            expect(result).to.include({name:"No Country for Old Men",year:2007,genre:"Western Drama",type:"feature",rating:9,content_rating:"R",cast_and_crew:["Josh Brolin","Tommy Lee Jones","Javier Bardem","Kelly MacDonald","Woody Harrelson","Joel Coen","Ethan Coen","Roger Deakins"]});
+                            done();
+                        });
+                });
+            });
+            describe('/movies/fuzzy/type/feature',function(){
+                it('should return (a) Movie object(s), the genre(s) of which is/are exactly the same as :substring_input, regardless of case', function(done) {
+                    supertest
+                        .get('/movies/fuzzy/type/feature')
+                        .end(function(err,res){
+                            expect(res.status).equal(200);
+                            expect(res.body).to.be.a('array');
+                            expect(res.body.length).equal(2);
+                            var result = _.map(res.body, function (movie) {
+                                return { name: movie.name, year: movie.year, genre: movie.genre, type: movie.type, rating: movie.rating, content_rating: movie.content_rating, cast_and_crew: movie.cast_and_crew }
+                            });
+                            expect(result).to.include({name:"Blade Runner",year:1982,genre:"Sci-fi Detective",type:"feature",rating:5,content_rating:"R",cast_and_crew:["Harrison Ford","Rutger Hauer","Ridley Scott","Vangelis","Hampton Fancher","Jordan Cronenweth"]});
+                            expect(result).to.include({name:"No Country for Old Men",year:2007,genre:"Western Drama",type:"feature",rating:9,content_rating:"R",cast_and_crew:["Josh Brolin","Tommy Lee Jones","Javier Bardem","Kelly MacDonald","Woody Harrelson","Joel Coen","Ethan Coen","Roger Deakins"]});
+                            done();
+                        });
+                });
+            });
+            describe('/movies/fuzzy/type/Short',function(){
+                it('should return an error message when :substring_input is not a substring of the genres of any Movie object', function(done){
+                    supertest
+                        .get('/movies/fuzzy/type/Short')
+                        .end(function(err,res){
+                            expect(res.status).equal(404);
+                            expect(res.body).to.be.a('object');
+                            expect(res.body).to.have.property('message');
+                            expect(res.body.message).equal("The substring search failed to return any Movie objects");
+                            done();
+                        });
+                });
+            });
+            describe('/movies/fuzzy/content_rating/R',function(){
+                it('should return all Movie objects, the content_ratings of which contain :substring_input as a substring, regardless of case', function(done){
+                    supertest
+                        .get('/movies/fuzzy/content_rating/R')
+                        .end(function(err,res){
+                            expect(res.status).equal(200);
+                            expect(res.body).to.be.a('array');
+                            expect(res.body.length).equal(2);
+                            var result = _.map(res.body, function (movie) {
+                                return { name: movie.name, year: movie.year, genre: movie.genre, type: movie.type, rating: movie.rating, content_rating: movie.content_rating, cast_and_crew: movie.cast_and_crew }
+                            });
+                            expect(result).to.include({name:"Blade Runner",year:1982,genre:"Sci-fi Detective",type:"feature",rating:5,content_rating:"R",cast_and_crew:["Harrison Ford","Rutger Hauer","Ridley Scott","Vangelis","Hampton Fancher","Jordan Cronenweth"]});
+                            expect(result).to.include({name:"No Country for Old Men",year:2007,genre:"Western Drama",type:"feature",rating:9,content_rating:"R",cast_and_crew:["Josh Brolin","Tommy Lee Jones","Javier Bardem","Kelly MacDonald","Woody Harrelson","Joel Coen","Ethan Coen","Roger Deakins"]});
+                            done();
+                        });
+                });
+            });
+            describe('/movies/fuzzy/content_rating/movies-Y',function(){
+                it('should return an error message when :substring_input is not a substring of the content_rating of any Movie object', function(done){
+                    supertest
+                        .get('/movies/fuzzy/content_rating/movies-Y')
+                        .end(function(err,res){
+                            expect(res.status).equal(404);
+                            expect(res.body).to.be.a('object');
+                            expect(res.body).to.have.property('message');
+                            expect(res.body.message).equal("The substring search failed to return any Movie objects");
+                            done();
+                        });
+                });
+            });
+        });
+
+        
     });
 });
